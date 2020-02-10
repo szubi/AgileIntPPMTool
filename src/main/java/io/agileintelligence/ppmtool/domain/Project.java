20173a773e3130a66f596430914bcf74d42f3371
szubi <w.szubi@gmail.com>
2020-02-10 18:18:29 +0100

    Set up project object validation

--- src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,9 +1,12 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,13 +17,28 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required")
     private String description;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private LocalDateTime startDate;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private LocalDateTime endDate;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private LocalDateTime updatedAt;
 
     @PrePersist
