d07d4b769980bc5aad12e6c824b68ac32f59edb1
szubi <w.szubi@gmail.com>
2020-02-10 17:44:28 +0100

    Add Project Entity, repository and lombok

--- src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,4 +1,35 @@
 package io.agileintelligence.ppmtool.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = LocalDate.now();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        this.updatedAt = LocalDate.now();
+    }
 }
