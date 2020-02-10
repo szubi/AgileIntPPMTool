abc1974d726eed2a05547b94707a920e1361ce5d
szubi <w.szubi@gmail.com>
2020-02-10 19:15:50 +0100

    Add project object validation

--- src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -1,27 +1,40 @@
 package io.agileintelligence.ppmtool.web;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.MapValidationErrorService;
 import io.agileintelligence.ppmtool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.Objects;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
     private ProjectService projectService;
+    private MapValidationErrorService mapValidationErrorService;
 
-    public ProjectController(ProjectService projectService) {
+    public ProjectController(ProjectService projectService, MapValidationErrorService mapValidationErrorService) {
         this.projectService = projectService;
+        this.mapValidationErrorService = mapValidationErrorService;
     }
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if (Objects.nonNull(errorMap)) {
+            return errorMap;
+        }
+
         projectService.saveOrUpdateProject(project);
 
-        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
     }
 }
